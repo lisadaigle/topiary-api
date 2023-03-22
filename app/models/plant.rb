cd8a620b332adafd93be5afa2cbbc0a51b9ba988
LISA DAIGLE <lisadaigle@gmail.com>
2023-03-21 23:16:26 -0400

    Fixed Login

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,3 +1,5 @@
 class Plant < ApplicationRecord
-  has_many :schedules 
+  has_many :schedules
+  has_many :user_plants
+  has_many :users, through: :user_plants
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_many :schedules
   has_secure_password
   validates :email, presence: true, uniqueness: true
+  has_many :user_plants
+  has_many :plants, through: :user_plants
 end
