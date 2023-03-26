27e3ca1bb125a215e8ad7d379dfe5cd2d52f98b6
LISA DAIGLE <lisadaigle@gmail.com>
2023-03-26 19:12:49 -0400

    Add Style to Plants Index

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+Plant.create(name: "Anthurium andraeanum (Coral Anthurium)",
+
+description: "Anthurium andraeanum, commonly called flamingo lily or painters palette, is native to Colombia and Ecuador. It is an epiphytic evergreen tropical perennial of the Arum family that is most noted for its attractive waxy, palette-shaped, bright red spathes and contrasting dark green foliage. It typically grows to 16” tall. Drooping, heart-shaped, dark green leaves (to 8” long) arise in a clump on long stems from the plant crown and are attractive year around. Each axillary flower (typical arum family) features a creamy yellow spadix enclosed by a large, flat, waxy red spathe (to 6”). Flowers are long-lasting. Flowers may appear throughout the year in optimum growing conditions. Fruits rarely appear on indoor plants.",
+amount_of_sun: 4,
+days_to_water: 7,
+image_url: "https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_coral-anthurium_small_bryant_cream_variant.jpg?v=1678208471") 
+
+
+
+Plant.create(
+name: "Ficus Lyrata (Fiddle Leaf Fig)", 
+description: "Ficus lyrata, commonly called fiddle-leaf fig or banjo fig, is native to Africa. It is a broadleaf evergreen tree that may grow to 60-100 ft tall in its native habitat. It is sometimes grown in the tropics as an ornamental shade tree. In colder climates, this is a popular houseplant that typically grows to 2-10 ft tall. It is noted for its large, lyre-shaped, dark green leaves (to 18” long) which resemble the shape of a violin (fiddle). In its native habitat, globular figs (to 1.25” diameter) appear solitary or in pairs on mature trees. Fruit is rarely seen on plants outside of the native habitat. Stems have a milky sap.", 
+amount_of_sun: 5, 
+ days_to_water: 14,
+image_url: "https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_Fiddle-Leaf-Fig-Tree_LargeGrant_Yellow.jpg?v=1678470114")
+
+
+Plant.create(
+  name: "Pellaea rotundifolia (Button Fern)", 
+  description: " Pellaea rotundifolia, commonly known as button fern, is a small evergreen fern with pinnate arching fronds rising and spreading to 12” tall and as wide. Each frond is covered with small, slightly-glossy, round, deep green pinnae (leaflets to 3/4” diameter). Leaf stalks turn a dark red with age. This fern is native to New Zealand, Australia and Norfolk Island where it is most frequently found growing on limestone cliffs, rocky crevices and moist open forested areas, but is occasionally found in drier woodland areas. North of Zone 9, it is primarily grown as a house plant in hanging baskets or in ornamental containers on tables.", 
+  amount_of_sun: 3, 
+  days_to_water: 10,
+  image_url: "https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_button-fern_medium_grant_terracotta.jpg?v=1679683917",)
+
+
+
+Plant.create(
+name: "Spathiphyllum (Peace Lily)",
+description: "Best grown in evenly moist but not soggy soil. Let the soil dry between waterings, but do not allow to wilt. When watering, use water that is at room temperature and has been allowed to sit so that the chlorine can evaporate. Spathiphyllum grow very well with sub-irrigation. These plants do well in lower light situations, but prefer bright filtered light. Do not place in direct sun.",
+amount_of_sun: 3,
+days_to_water: 14,
+image_url: "https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_Peace-Lily-Domino_MediumHyde_Cream.jpg?v=1678826402"
+)
+
+
+Plant.create(
+name: "Monstera deliciosa (Swiss Cheese Plant)",
+description: "Monstera deliciosa, commonly called split-leaf philodendron or swiss cheese plant, is native to Central America. It is a climbing, evergreen perennial vine that is perhaps most noted for its large perforated leaves on thick plant stems and its long cord-like aerial roots. In its native tropical habitat, it will climb somewhat impressively to 70 ft into large trees, clothing the trunks with leaves in the 1-3ft long range. Indoor plants more typically are grown in the 6-8 ft range. Mature leaves of this plant are very large, glossy, deep green and distinctively cut and perforated.",
+amount_of_sun: 4,
+days_to_water: 7,
+image_url: "https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_monstera_variant_medium_hyde_mint_f0e6d601-426c-40fe-abc6-b0a1f4dce17b.jpg?v=1672212220"
+)
